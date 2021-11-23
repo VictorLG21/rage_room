6342b33e2cd6fc6a346e197ead34a5191b244ce6
João Kitajima <joao.kitajima@outlook.com>
2021-11-23 17:31:21 -0300

    maven setup

---
+++ rage_room/src/main/java/rage/room/App.java
@@ -0,0 +1,13 @@
+package rage.room;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
