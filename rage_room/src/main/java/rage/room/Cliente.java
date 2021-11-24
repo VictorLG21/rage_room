570167f462be41317d7025c074c5943041b488e8
João Kitajima <joao.kitajima@outlook.com>
2021-11-24 12:45:06 -0300

    'Cliente' class updated

--- rage_room/src/main/java/rage/room/Cliente.java
+++ rage_room/src/main/java/rage/room/Cliente.java
@@ -17,42 +17,42 @@ public class Cliente {
 
 
     /* MÉTODOS */
-    public boolean validarIdade(int diaNascimento, int mesNascimento, int anoNascimento) {
+    public static boolean validarIdade(int diaNascimento, int mesNascimento, int anoNascimento) {
         Date data = new Date();
         LocalDate dataLocal = data.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
         int diaAtual = dataLocal.getDayOfMonth();
         int mesAtual = dataLocal.getMonthValue();
         int anoAtual = dataLocal.getYear();
 
-        if(anoAtual - anoNascimento >= 18 && mesAtual - mesNascimento >= 0 && diaAtual - diaNascimento >= 0) {
+        if((diaNascimento >= 1 && diaNascimento <= 31) && (mesNascimento >= 1 && mesNascimento <= 12) && (anoNascimento >= 1900 && anoNascimento <= 2021) && (anoAtual - anoNascimento >= 18 && mesAtual - mesNascimento >= 0 && diaAtual - diaNascimento >= 0)) {
             return true;
         } else {
             return false;
         }
     }
 
-    public boolean validarNome(String nome) {
-        if(nome.length() >= 2 &&  nome.length() <= 45 && nome.matches("[a-zA-Z\u00C0-\u017F]+")) {
+    public static boolean validarNome(String nome) {
+        if(nome.length() >= 2 &&  nome.length() <= 45 && nome.matches("[a-zA-Z \u00C0-\u017F]+")) {
             return true;
         } else {
             return false;
         }
     }
 
-    public boolean validarProfissao(String profissao) {
-        if(profissao.length() >= 3 &&  profissao.length() <= 45 && profissao.matches("[a-zA-Z\u00C0-\u017F]+")) {
+    public static boolean validarProfissao(String profissao) {
+        if(profissao.length() >= 3 &&  profissao.length() <= 45 && profissao.matches("[a-zA-Z \u00C0-\u017F]+")) {
             return true;
         } else {
             return false;
         }
     }
 
-    public boolean validarRG(String rg) {
+    public static boolean validarRG(String rg) {
         if(rg.length() == 9) {
             try {
                 Integer.parseInt(rg);
             } catch (Exception e) {
-                throw new NumberFormatException("Formato inválido do RG.");
+                throw new NumberFormatException("RG INVÁLIDO!");
             }
 
             return true;
